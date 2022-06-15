09df4cc9119ae9f096f26d53eec0a533ab8efa74
Santosh Magar <24504319+mgrsantox@users.noreply.github.com>
2022-06-15 10:34:24 +0545

    Update settings.py

--- geoexport/settings.py
+++ geoexport/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kr9s^&(55n--y#1z&nbftg2wk@6&lnc54&6%hfyxxcn7m@y^zv'
+SECRET_KEY = 'YOUR_SECRECT_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
